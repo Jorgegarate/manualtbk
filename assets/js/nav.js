f0c18308614a527cf82426da6ea9b7280cb90342
catalinalazo <catalina.lazo@siigroup.cl>
2022-11-11 12:42:41 -0300

    scroll celu

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,19 +2,33 @@ const menuBtn = document.querySelector('.menu-btn');
 const navColor = document.querySelector('.nav--color');
 const scrollContent = document.querySelector('.scroll');
 const mainContent = document.querySelector('.main');
+const menuClose = document.querySelector('.btn--close');
+const media = window.matchMedia("(max-width: 992px)")
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('active');
-    navColor.classList.add('active')
-    scrollContent.classList.add('active')
-    mainContent.classList.add('active')
+    navColor.classList.add('active');
+    mainContent.classList.add('active');
+    menuClose.classList.add('active');
     menuOpen = true;
+    if(!media){
+      scrollContent.classList.add('active');
+    }
   } else {
     menuBtn.classList.remove('active');
-    navColor.classList.remove('active')
-    scrollContent.classList.remove('active')
-    mainContent.classList.remove('active')
+    navColor.classList.remove('active');
+    scrollContent.classList.remove('active');
+    mainContent.classList.remove('active');
     menuOpen = false;
   }
+
 });
+
+menuClose.addEventListener('click', () => {
+    console.log("click")
+      menuClose.classList.remove('active');
+      scrollContent.classList.remove('active');
+      navColor.classList.remove('active');
+});
+
