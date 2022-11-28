c73026cddb1343e4a143b9256e5cd84c69955ead
Jorge Garate <jorgeluis.garate@gmail.com>
2022-11-28 12:44:37 -0300

    modificacion del menu con veil

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -4,9 +4,9 @@ const scrollContent = document.querySelector('.scroll');
 const accordionButton = document.querySelectorAll('.accordion-button');
 const mainContent = document.querySelector('.main');
 const buttonAccordionCollapse = document.querySelectorAll('.accordion-collapse');
-const menuClose = document.querySelector('.btn--close');
 const media = window.matchMedia("(min-width: 992px)");
 const accordionCollapse= document.querySelector(".accordion-collapse");
+const blackVeil =document.querySelector('.black--veil')
 
 let menuOpen = false;
 
@@ -20,13 +20,17 @@ menuBtn.addEventListener('click', () => {
     menuOpen = true
 
     if(window.innerWidth>=992){
-      menuClose.classList.add('active');
       scrollContent.classList.add('active');
       accordionCollapse.classList.add('none');
       accordionButton.forEach(element => {element.classList.add('active')
       element.classList.remove('collapsed')})
       buttonAccordionCollapse.forEach(element =>{element.classList.add('none')});
-  } 
+    }
+    else {
+        if (window.innerWidth<992){
+        blackVeil.classList.add('active')
+      }
+    } 
 }
   else { 
     menuBtn.classList.remove('active');
@@ -35,22 +39,11 @@ menuBtn.addEventListener('click', () => {
     mainContent.classList.remove('active');
     accordionCollapse.classList.remove('none')
     accordionButton.forEach(element =>{element.classList.remove('active')})
+    blackVeil.classList.remove('active')
     menuOpen = false;
   }
 
 
-});
-
-menuClose.addEventListener('click', () => {
-      menuClose.classList.remove('active');
-      scrollContent.classList.remove('active');
-      navColor.classList.remove('active');
-      mainContent.classList.remove('active');
-      if(window.innerWidth>=992){
-        scrollContent.classList.add('active');
-        accordionCollapse.classList.remove('none')
-      }
-      menuOpen = false;
 });
 
 navColor.addEventListener('click', () => {
@@ -62,13 +55,19 @@ navColor.addEventListener('click', () => {
 
   } else {
     menuOpen = false;
-    menuClose.classList.remove('active');
     mainContent.classList.remove('active');
     scrollContent.classList.remove('active');
     mainContent.classList.remove('active');
     buttonAccordionCollapse.forEach(element =>{element.classList.remove('none')})
     
   }
+});
+blackVeil.addEventListener('click', () => {
+    navColor.classList.remove('active');
+    menuBtn.classList.remove('active');
+    blackVeil.classList.remove('active')
+    menuOpen = false;
+
 });
 
 
